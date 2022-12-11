37e1e79d0e94813859ae901edb87b339a9282590
Yiğit Demirşan <59603439+YigitDemirsan@users.noreply.github.com>
2022-12-11 21:22:33 +0300

    Update ChomskyNormalForm.java

--- ChomskyNormalForm.java
+++ ChomskyNormalForm.java
@@ -4,6 +4,166 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+
+class ContextFreeGrammar {
+    private Scanner scanner;
+    private ArrayList<String> left_most;
+    private ArrayList<List<String>> right_most;
+    private ArrayList<String> terminal_side;
+    private ArrayList<String> non_terminal_side;
+    boolean is_contain_epsilon = false;
+
+    ContextFreeGrammar(File input) throws FileNotFoundException {
+        this.scanner = new Scanner(input);
+        this.left_most = new ArrayList<>();
+        this.right_most = new ArrayList<>();
+        this.terminal_side = new ArrayList<>();
+        this.non_terminal_side = new ArrayList<>();
+        this.parse_words();
+        this.merge_words();
+    }
+
+    public ArrayList<String> epsilon_non_terminals(){
+        ArrayList<String> empty_String = new ArrayList<String>();
+        for (int i = 0; i < this.right_most.size(); i++){
+            if (this.right_most.get(i).contains("0"))
+                empty_String.add(this.left_most.get(i));
+        }
+        boolean is_new = true;
+        do{
+            is_new = false;
+            for (int i = 0; i < this.right_most.size(); i++){
+                List<String> get = this.right_most.get(i);
+                for (int k = 0; k < get.size(); k++) {
+                    String word = get.get(k);
+                    boolean all_epsilon = true;
+                    char[] charArray = word.toCharArray();
+                    for (int j = 0; j < charArray.length; j++) {
+                        char c = charArray[j];
+                        if (!empty_String.contains(String.valueOf(c))) {
+                            all_epsilon = false;
+                            break;
+                        }
+                    }
+                    if (all_epsilon && !empty_String.contains(this.left_most.get(i))) {
+                        is_new = true;
+                        empty_String.add(this.left_most.get(i));
+                        break;
+                    }
+                }
+            }
+        }while(is_new);
+        return empty_String;
+    }
+
+    public void add_new_rule(String left_side, List<String> right_most){
+        this.left_most.add(left_side);
+        this.right_most.add(right_most);
+    }
+
+    public void rewrite_rule(String left, List<String> right_most){
+        this.right_most.get(this.left_most.indexOf(left)).clear();
+        this.right_most.get(this.left_most.indexOf(left)).addAll(right_most);
+    }
+
+    public void change_left_side(String old_element, String new_element){
+        this.left_most.set(this.left_most.indexOf(old_element), new_element);
+    }
+
+    public String free(){
+        char c = 'A';
+        do {
+            if (!this.non_terminal_side.contains(String.valueOf(c)))
+                return String.valueOf(c);
+            c++;
+        }while (c < 'Z');
+        return "A";
+    }
+
+    private void parse_words(){
+        while (this.scanner.hasNext()){
+            List<String> line = Arrays.asList(scanner.nextLine().split(" "));
+            this.left_most.add(line.get(0));
+            this.right_most.add(new ArrayList<>());
+            this.right_most.get(this.right_most.size() - 1).addAll(line.subList(1, line.size()));
+            for (int i = 0; i < line.size(); i++) {
+                String s = line.get(i);
+                char[] charArray = s.toCharArray();
+                for (int j = 0; j < charArray.length; j++) {
+                    char c = charArray[j];
+                    if (c == '0') {
+                        this.is_contain_epsilon = true;
+                    } else if (String.valueOf(c).toUpperCase().equals(String.valueOf(c))) {
+                        String t = String.valueOf(c);
+                        if (!this.non_terminal_side.contains(t))
+                            this.non_terminal_side.add(t);
+                    } else {
+                        String t = String.valueOf(c);
+                        if (!this.terminal_side.contains(t))
+                            this.terminal_side.add(String.valueOf(c));
+                    }
+                }
+            }
+        }
+    }
+    public void merge_words(){
+        ArrayList<Integer> removable_elements = new ArrayList<>();
+        for (int i = 0; i < this.left_most.size(); i++){
+            for (int j = i + 1; j < this.left_most.size(); j++){
+                if (this.left_most.get(i).equals(this.left_most.get(j))){
+                    this.right_most.get(i).addAll(this.right_most.get(j));
+                    removable_elements.add(j);
+                }
+            }
+        }
+        int k = 0;
+        for (int i = 0 ; i<removable_elements.size() ; i++){
+            this.left_most.remove(i - k);
+            this.right_most.remove(i - k);
+            k = k+ 1;
+        }
+    }
+
+    public void print_word(){
+        for (int i = 0; i < this.right_most.size(); i++){
+            System.out.print(this.left_most.get(i) + ": ");
+            for (String word: this.right_most.get(i)){
+                System.out.print(word + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public void setLeft_most(ArrayList<String> left_most) {
+        this.left_most = left_most;
+    }
+
+    public void setRight_most(ArrayList<List<String>> right_most) {
+        this.right_most = right_most;
+    }
+
+    public void setTerminal_side(ArrayList<String> terminal_side) {
+        this.terminal_side = terminal_side;
+    }
+
+    public void setNon_terminal_side(ArrayList<String> non_terminal_side) {
+        this.non_terminal_side = non_terminal_side;
+    }
+
+    public void setIs_contain_epsilon(boolean is_contain_epsilon) {
+        this.is_contain_epsilon = is_contain_epsilon;
+    }
+
+    public List<String> get_all_terminals() { return terminal_side; }
+    public List<String> get_all_nonterminals() { return non_terminal_side; }
+    public List<String> all_rules(String t) { return this.right_most.get(this.left_most.indexOf(t)); }
+    public List<String> get_left_side() { return left_most; }
+    public List<List<String>> get_right_side() { return right_most; }
+}
 public class ChomskyNormalForm {
     private ContextFreeGrammar grammar;
 
@@ -35,7 +195,7 @@ public class ChomskyNormalForm {
             }
         }
         int i = 0;
-        List<List<String>> right = this.grammar.getRight();
+        List<List<String>> right = this.grammar.get_right_side();
         for (int i2 = 0; i2 < right.size(); i2++) {
             List<String> words = right.get(i2);
             i = i + 1;
@@ -53,7 +213,7 @@ public class ChomskyNormalForm {
                     }
                 }
             }
-            this.grammar.getRight().get(i - 1).removeAll(remove);
+            this.grammar.get_right_side().get(i - 1).removeAll(remove);
         }
         ArrayList<Integer> remove = new ArrayList<>();
         int k = 0;
@@ -69,7 +229,7 @@ public class ChomskyNormalForm {
         for (int i1 = 0; i1 < remove.size(); i1++) {
             int j = remove.get(i1);
             this.grammar.get_all_nonterminals().remove(this.grammar.get_left_side().get(j - k));
-            this.grammar.getRight().remove(j - k);
+            this.grammar.get_right_side().remove(j - k);
             this.grammar.get_left_side().remove(j - k);
             k = k + 1;
         }
@@ -101,16 +261,16 @@ public class ChomskyNormalForm {
         }
         System.out.println();
         for (int i = 0; i < this.grammar.get_left_side().size(); i++){
-            for (int j = 0; j < this.grammar.getRight().get(i).size(); j++){
-                String word = this.grammar.getRight().get(i).get(j);
+            for (int j = 0; j < this.grammar.get_right_side().get(i).size(); j++){
+                String word = this.grammar.get_right_side().get(i).get(j);
                 char[] charArray = word.toCharArray();
                 for (int k = 0; k < charArray.length; k++) {
                     char c = charArray[k];
                     if (epsilon.contains(String.valueOf(c))) {
                         if (word.length() == 1) {
-                            this.grammar.getRight().get(i).add("0");
+                            this.grammar.get_right_side().get(i).add("0");
                         } else {
-                            this.grammar.getRight().get(i).add(word.replaceFirst(String.valueOf(c), ""));
+                            this.grammar.get_right_side().get(i).add(word.replaceFirst(String.valueOf(c), ""));
                         }
                         break;
                     }
@@ -130,7 +290,7 @@ public class ChomskyNormalForm {
         for (int i = 0; i < remove.size(); i++) {
             String left = remove.get(i);
             this.grammar.get_all_nonterminals().remove(left);
-            this.grammar.getRight().remove(this.grammar.get_left_side().indexOf(left));
+            this.grammar.get_right_side().remove(this.grammar.get_left_side().indexOf(left));
             this.grammar.get_left_side().remove(left);
         }
         this.grammar.print_word();
@@ -142,7 +302,7 @@ public class ChomskyNormalForm {
             String non_terminal = left_side.get(j);
             if (non_terminal.equals("S")) continue;
             boolean finded = false;
-            List<List<String>> right = this.grammar.getRight();
+            List<List<String>> right = this.grammar.get_right_side();
             for (int i = 0; i < right.size(); i++) {
                 List<String> words = right.get(i);
                 for (String word : words) {
@@ -171,7 +331,7 @@ public class ChomskyNormalForm {
             for (int j = 0; j < grammarLeft.size(); j++) {
                 String left = grammarLeft.get(j);
                 if (inaccessible.contains(left)) {
-                    this.grammar.getRight().remove(i - removed);
+                    this.grammar.get_right_side().remove(i - removed);
                     removed += 1;
                 }
                 i += 1;
@@ -186,7 +346,7 @@ public class ChomskyNormalForm {
     }
     private void is_start_symbol(){
         boolean need_to_add = false;
-        for (List<String> rules: this.grammar.getRight()) {
+        for (List<String> rules: this.grammar.get_right_side()) {
             for (String rule : rules) {
                 if (rule.contains("S")) {
                     need_to_add = true;
